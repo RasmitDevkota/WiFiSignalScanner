2f7e1b692687573e9d510b1c0f6f58ce99f8e0f6
Rasmit Devkota <alientechgamig@hotmail.com>
2022-05-25 22:00:21 -0400

    settings popup (wip)

--- app/src/main/java/com/alientech/wifisignalscanner/MainActivity.kt
+++ app/src/main/java/com/alientech/wifisignalscanner/MainActivity.kt
@@ -2,6 +2,8 @@ package com.alientech.wifisignalscanner
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
@@ -10,11 +12,16 @@ import android.net.wifi.WifiManager
 import android.net.wifi.WifiNetworkSpecifier
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.os.PatternMatcher
 import android.text.method.ScrollingMovementMethod
+import android.view.Gravity
 import android.view.View
-import android.widget.TextView
+import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
+import java.io.File
+import java.io.FileOutputStream
 import java.sql.Timestamp
 import java.time.Instant
 import java.util.*
@@ -28,13 +35,18 @@ class MainActivity : Activity() {
     val ssid = "ASUS"
 
     val rssiList = ArrayList<Int>()
+    lateinit var rssiString: String
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // UI
+
         findViewById<TextView>(R.id.rssiText).movementMethod = ScrollingMovementMethod()
+
+        // WiFi
 
         wifi = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         connectivity = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -71,13 +83,66 @@ class MainActivity : Activity() {
 
                     val average = rssiList.average().roundToInt()
 
-                    val rssiText = findViewById<View>(R.id.rssiText) as TextView
-                    rssiText.text = "${rssiText.text}($timestring) RSSI: $rssi dBm; Average RSSI: $average dBm\n"
+                    rssiString += "($timestring) RSSI: $rssi dBm; Average RSSI: $average dBm\n"
+                    (findViewById<View>(R.id.rssiText) as TextView).text = rssiString
                 }
             }
         }
 
         t.scheduleAtFixedRate(tt, 500, 1000)
+
+        // Settings
+        val settingsButton = findViewById<ImageButton>(R.id.settings_button)
+
+        settingsButton.setOnClickListener {
+            val inflater = layoutInflater
+            val settingsPopupView = inflater.inflate(R.layout.settings_popup, null)
+
+            val width = LinearLayout.LayoutParams.WRAP_CONTENT
+            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+            val focusable = true
+
+            val popupWindow = PopupWindow(settingsPopupView, width, height, focusable).apply {
+                showAtLocation(View(applicationContext), Gravity.CENTER, 0, 0)
+                println("hi")
+            }
+
+            settingsPopupView.findViewById<Button>(R.id.buttonPopup).setOnClickListener {
+
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun saveData() {
+        if (Environment.MEDIA_MOUNTED != Environment.getExternalStorageState()) {
+            return
+        }
+
+        val file = File(getExternalFilesDir(null), "")
+
+        try {
+            file.createNewFile()
+
+            FileOutputStream(file, true).apply {
+                write(rssiString.toByteArray())
+
+                flush()
+
+                close()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+
+            TODO("Display error to user")
+        }
+    }
+
+    fun copyTXT() {
+        val txtData = rssiString
+        val clipboard = getSystemService(applicationContext, ClipboardManager::class.java)
+        val clip = ClipData.newPlainText("", rssiString)
+        clipboard!!.setPrimaryClip(clip)
     }
 }
 
